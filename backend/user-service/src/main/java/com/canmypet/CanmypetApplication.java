6360693f8f7d73280a2c3b146fb3c029a3555b1d
Kattya Herrera <kattyahs@gmail.com>
2026-08-25 11:01:15 -0400

    refactor: restructure backend into microservices (user-service, pet-service, food-service, api-gateway)

---
+++ backend/user-service/src/main/java/com/canmypet/CanmypetApplication.java
@@ -0,0 +1,12 @@
+package com.canmypet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanmypetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CanmypetApplication.class, args);
+    }
+}
