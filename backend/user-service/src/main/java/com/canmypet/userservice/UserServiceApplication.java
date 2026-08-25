d5d61f0d88c95e1895a5d91c8cf281741f482f52
Kattya Herrera <kattyahs@gmail.com>
2026-08-25 11:08:48 -0400

    refactor: rename user-service package and config for microservices architecture

--- backend/user-service/src/main/java/com/canmypet/userservice/UserServiceApplication.java
+++ backend/user-service/src/main/java/com/canmypet/userservice/UserServiceApplication.java
@@ -1,12 +1,12 @@
-package com.canmypet;
+package com.canmypet.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CanmypetApplication {
+public class UserServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CanmypetApplication.class, args);
+        SpringApplication.run(UserServiceApplication.class, args);
     }
 }
